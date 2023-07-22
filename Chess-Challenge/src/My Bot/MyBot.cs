ece88d1d08e3b18953ee529872f6148c8ccd89c1
artheii <temka19977@mail.ru>
2023-07-23 00:00:46 +0200

    transpositionTable

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,11 +1,34 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using static System.Formats.Asn1.AsnWriter;
 
 public class MyBot : IChessBot
 {
+    private int foundCounter = 0;
+    enum EntryType
+    {
+        Exact,
+        LowerBound,
+        UpperBound
+    }
+
+    private class TranspositionTableEntry
+    {
+        public float Evaluation { get; set; }
+        public int Depth { get; set; }
+
+        public EntryType EntryType { get; set; }
+    }
+
+    private Dictionary<ulong, TranspositionTableEntry> transpositionTable = new();
+
     public Move Think(Board board, Timer timer)
     {
+        System.Console.WriteLine("Table size");
+        System.Console.WriteLine(transpositionTable.Count);
+        System.Console.WriteLine("Counter");
+        System.Console.WriteLine(foundCounter);
         return FindTheBestMove(board);
     }
 
@@ -15,6 +38,7 @@ public class MyBot : IChessBot
         {
             return -90000;
         }
+
 
         float[] weights = { 100, 305, 333, 563, 950 };
         var allPiecesCounts = board.GetAllPieceLists().Select(x => x.Count).ToList();
@@ -40,7 +64,7 @@ public class MyBot : IChessBot
         foreach (var move in board.GetLegalMoves())
         {
             board.MakeMove(move);
-            var evaluation = -Negamax(board, 3, -90000, 90000);
+            var evaluation = -Negamax(board, 4, -90000, 90000);
             board.UndoMove(move);
             if (evaluation > maxEval)
             {
@@ -55,22 +79,55 @@ public class MyBot : IChessBot
     private float Negamax(Board board, int depth, float alpha, float beta)
     {
         var legalMoves = board.GetLegalMoves();
+
+        var ttEntry = transpositionTable.GetValueOrDefault(board.ZobristKey);
+        if (ttEntry != null && ttEntry.Depth >= depth)
+        {
+            foundCounter++;
+            if (ttEntry.EntryType == EntryType.Exact)
+            {
+                return ttEntry.Evaluation;
+            } else if(ttEntry.EntryType == EntryType.LowerBound)
+            {
+                alpha = System.Math.Max(alpha, ttEntry.Evaluation);
+            } else if(ttEntry.EntryType == EntryType.UpperBound)
+            {
+                beta = System.Math.Min(beta, ttEntry.Evaluation);
+            }
+        }
+
         if (depth == 0 || legalMoves.Length == 0)
             return Quiesce(board, alpha, beta);
 
+        var value = -90000f;
         foreach (var move in legalMoves)
         {
             board.MakeMove(move);
-            var newEvaluation = -Negamax(board, depth - 1, -beta, -alpha);
+            value = Math.Max(value, -Negamax(board, depth - 1, -beta, -alpha));
             board.UndoMove(move);
-            if (newEvaluation >= beta)
-                return beta;
-            if(newEvaluation > alpha)
-            {
-                alpha = newEvaluation;
-            }
+            alpha = Math.Max(alpha, value);
+            if (alpha >= beta)
+                break;
         }
-        return alpha;
+
+        var entry = new TranspositionTableEntry
+        {
+            Evaluation = value,
+            Depth = depth,
+            EntryType = EntryType.Exact
+        };
+
+        if(value <= alpha)
+        {
+            entry.EntryType = EntryType.UpperBound;
+        } else if(value >= beta)
+        {
+            entry.EntryType = EntryType.LowerBound;
+        }
+
+        transpositionTable[board.ZobristKey] = entry;
+
+        return value;
     }
 
     private float Quiesce(Board board, float alpha, float beta)
@@ -81,19 +138,16 @@ public class MyBot : IChessBot
         if (alpha < standPat)
             alpha = standPat;
 
-        foreach (var move in board.GetLegalMoves())
+        foreach (var move in board.GetLegalMoves(true))
         {
-            if (move.IsCapture)
-            {
-                board.MakeMove(move);
-                var score = -Quiesce(board, -beta, -alpha);
-                board.UndoMove(move);
+            board.MakeMove(move);
+            var score = -Quiesce(board, -beta, -alpha);
+            board.UndoMove(move);
 
-                if (score >= beta)
-                    return beta;
-                if (score > alpha)
-                    alpha = score;
-            }
+            if (score >= beta)
+                return beta;
+            if (score > alpha)
+                alpha = score;
         }
         return alpha;
     }
